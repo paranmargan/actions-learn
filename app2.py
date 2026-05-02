14eff00da2ea2d732d6a410e3ea0c8ebc36cde8b
psparan1980 <psparan1980@gmail.com>
2026-05-02 20:54:04 +0530

    update to python yaml file

---
+++ app2.py
@@ -0,0 +1,16 @@
+import os
+name = os.getenv("NAME" ,"python_example")
+run_tests = os.getenv("RUN_TESTS", "false").lower() == "true"
+log_level = os.getenv("LOG_LEVEL", "info")
+target_environment = os.getenv("TARGET_ENVIRONMENT", "dev")
+
+
+print("Python script started")
+print(f"Name: {name}")
+print(f"Run tests: {run_tests}")
+print(f"Repeat count: {repeat_count}")
+print(f"Log level: {log_level}")
+print(f"Target environment: {target_environment}")
+
+for i in range(repeat_count):
+    print(f"{i + 1}. Hello, {name}!")

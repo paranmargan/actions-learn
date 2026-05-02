5284693b549cbc05e14a903f7863f17db6da4c43
psparan1980 <psparan1980@gmail.com>
2026-05-02 21:05:20 +0530

    update to app2.py

--- app2.py
+++ app2.py
@@ -1,9 +1,9 @@
 import os
-name = os.getenv("NAME" ,"python_example")
+name = os.getenv("NAME", "python_example")
 run_tests = os.getenv("RUN_TESTS", "false").lower() == "true"
 log_level = os.getenv("LOG_LEVEL", "info")
 target_environment = os.getenv("TARGET_ENVIRONMENT", "dev")
-
+repeat_count = int(os.getenv("REPEAT_COUNT", "1"))
 
 print("Python script started")
 print(f"Name: {name}")
